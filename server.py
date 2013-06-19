022ce7f61a896fbd7b03b895de067731bba81a51
Louis Taylor <kragniz@gmail.com>
2013-06-20 00:23:04 +0100

    Added a ranked page

--- server.py
+++ server.py
@@ -1,19 +1,21 @@
 import web
 import random
 import json
+from operator import itemgetter
 
 photos = json.load(open('photos.db'))
 
-def photo_url(item):
+def photo_url(item, size='z'):
     return 'http://farm{farm}.staticflickr.com/{server}/{id}_{secret}_{size}.jpg'.format(
             farm = item['farm'],
             server = item['server'],
             id = item['id'],
             secret = item['secret'],
-            size = 'z')
+            size = size)
 
 urls = (
     '/', 'Index',
+    '/ranks', 'Ranks'
 )
 
 class Index(object):
@@ -65,6 +67,38 @@ body {{
             json.dump(photos, f,
                       sort_keys=True, indent=4, separators=(',', ': '))
         raise web.seeother('/')
+
+class Ranks(object):
+    def GET(self):
+        def div(a, b):
+            if b == 0:
+                return a
+            else:
+                return a / float(b)
+
+        p = json.load(open('photos.db'))
+        ranked = []
+
+        for i, photo in enumerate(p):
+            score = div(photo['up'], photo['down'])
+            photo['score'] = score
+            if score > 0:
+                ranked += [photo]
+        sortedranked = sorted(ranked, key=itemgetter('score'), reverse=True)
+        html = '''<html>
+<style type="text/css">
+body {
+    background-color:#050505;
+  }
+</style>
+'''
+        for photo in sortedranked:
+            html += '    <a href=\
+"http://www.flickr.com/photos/96822206@N03/{imageid}">\
+<img src="{thumb}"></a>\n'.format(imageid = photo['id'],
+                                thumb = photo_url(photo, size='m'))
+        return html + '</html>'
+
 
 if __name__ == "__main__":
     app = web.application(urls, globals())
